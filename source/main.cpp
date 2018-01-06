72a39a7146cf34b1b765902a2c246d5e444bfd56
pierrebonny <pbonny63@free.fr>
2018-01-06 18:55:11 +0100

    random

--- source/main.cpp
+++ source/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main (int argc, char *argv[]){
     string filename;
+    std::queue<int> valuesQueue;
     if(argc > 1){
         filename = argv[1];
     }else{
@@ -36,5 +37,16 @@ int main (int argc, char *argv[]){
     WorstFit worstFit(parser.getQueue(),parser.getBinSize());
     worstFit.compute();
     worstFit.dispResult();
+
+    srand(time(0));
+    cout<<"entrez la contenance maximum des boites"<<endl;
+    int b_size;
+    cin>>b_size;
+    cout<<"entrez le nombre de valeurs que vous souhaitez générer"<<endl;
+    int boxNumbers;
+    cin>>boxNumbers;
+    for(int i = 0; i < boxNumbers;i++){
+        valuesQueue.push(rand()%b_size);
+    }
     return 0;
 }
