caf23486cd88640cc42e6c4ae0b59b799454f7c2
Gaetan Vialon <gaetan.vialon@etu.unice.fr>
2018-01-06 19:13:13 +0100

    deso j'avais enlevé des trucs

--- source/main.cpp
+++ source/main.cpp
@@ -24,8 +24,8 @@ int main (int argc, char *argv[]){
     }
     cout<<"BEST FIT"<< endl;
     BestFit bestFit(parser.getQueue(),parser.getBinSize());
-    //bestFit.compute();
-    //bestFit.dispResult();
+    bestFit.compute();
+    bestFit.dispResult();
     cout<<"NEXT FIT"<< endl;
     NextFit nextFit(parser.getQueue(),parser.getBinSize());
     nextFit.compute();
@@ -36,7 +36,7 @@ int main (int argc, char *argv[]){
     firstFit.dispResult();
     cout<<"WORST FIT"<< endl;
     WorstFit worstFit(parser.getQueue(),parser.getBinSize());
-    //worstFit.compute();
-    //worstFit.dispResult();
+    worstFit.compute();
+    worstFit.dispResult();
     return 0;
 }
